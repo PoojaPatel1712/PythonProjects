6c422cdac8d430db8e323ef0a91d5713fba8f7f8
Pooja Patel <128218158+PoojaPatel1712@users.noreply.github.com>
2024-01-13 00:02:57 +0530

    Create 1.Band_Name_Generator.py

---
+++ AssignmentProjects/1.Band_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator")
+
+# = Assignment Operator
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("Wha is thename of your pet?\n")
+
+print("Your ban name could be " + city_name + " " + pet_name)
